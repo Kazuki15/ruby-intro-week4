24dfef0c4941757435bce1479356d9bd68080f26
Kazuki15 <kazuki.ishikawa@kellogg.northwestern.edu>
2026-04-24 14:25:53 +0000

    update 2-condition

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,57 @@
 # ruby 2-conditionals.rb
 
 # Booleans
-
+# True
+# false
 # Boolean Expressions
-
+# not just "="
+x = 3
+y = 4
+puts x == y
+puts x != y
+puts x>= y
 # If Conditional Logic
+# if condition
+#   ... do something
+#   end
+
+if x == y
+  puts "x == y, yay!"
+end
+
+if x != y
+  puts "x == y, yay!"
+end
+
+if x == y
+  puts "x == y, yay!"
+end
+
+if x == y
+  puts "x == y, yay!"
+else
+ puts "phew, math works!"
+
+end
 
 # If/Else Conditional Logic
 
 # Elsif Conditional Logic
+home_team = 2
+away_team = 3
+
+#if home beats away
+
+if home_team > away_team
+  puts "WE WON!!"
+elsif away_team > home_team
+  puts "WE LOST...boooo!"
+else
+  puts "Could be worse..."
+end
+
 
 # Combining Expressions
+today = "saturday"
+ puts (home_team == away_team) && (today == "Saturday")
+ puts (home_team == away_team) || (today == "Saturday")
