e53c746bd5b4269d7d699f02213bb4b2e2557161
Kazuki15 <kazuki.ishikawa@kellogg.northwestern.edu>
2026-04-24 15:26:53 +0000

    3-4 updated

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -3,10 +3,29 @@
 # ruby 3-arrays.rb
 
 # Arrays are lists of things
+foods = ["tacos", "pizza", 'ice cream']
+puts foods
+p foods
+
+numbers = [4, 8, 15, 16, 23, 42]
+p numbers
+
+ mixed = ["tacos", 3, true]
 
 # Accessing the array
+p foods[0]
+p foods[2]
+p foods[3]
 
 # Add to the array
+
+# p foods + numbers
+# p foods + ["avocados"]
+foods << "avocados"
+p foods
+foods.push("avocados")
+
+p foods[3]
 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,18 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+profile = {"name" => "Ben", 
+"location" => "Evanston",
+"status" => "Teaching ENTR-451"}
+p profile
 
 # Accessing data from the hash
+puts profile["name"]
+puts profile["location"]
 
+profile["status"] = {"description" => "Teaching", "time" => "10:20am"}
+profile["age"] = 44
+
+p profile
+ puts profile["status"]["time"]
 # More Complex Hashes
